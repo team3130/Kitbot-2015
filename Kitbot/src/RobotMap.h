536411a5f49b9f2cb56a92ff040f79123236390b
Mikhail Kyraha <mikhail@kyraha.com>
2015-02-05 00:33:01 -0600

    Robotmap reordered
    No functional changes, only the order of the lines for easier ports
    assignments.

--- Kitbot/src/RobotMap.h
+++ Kitbot/src/RobotMap.h
@@ -9,47 +9,57 @@
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
- 
-// For example to map the left and right motors, you could define the
-// following variables to use with your drivetrain subsystem.
-//Drive Train/Chassis
+
+/****** PWM ports ************************/
+
 const int LEFTBACKMOTOR = 0;
 const int RIGHTBACKMOTOR = 1;
 const int LEFTFRONTMOTOR = 2;
 const int RIGHTFRONTMOTOR = 3;
+//Intake
+const int INTAKER = 8;
+const int INTAKEL = 9;
+
+
+/****** Digital I/O **********************/
+
+//Moose
+const int MOOSEPOSITIONSENSOR = 0;		//Change to Digital Input Output port 0
+										//if available. Chris wired it there
 const int DRIVE_ENCODERL_A = 4;
 const int DRIVE_ENCODERL_B = 5;
 const int DRIVE_ENCODERR_A = 6;
 const int DRIVE_ENCODERR_B = 7;
 
-//Intake
-const int INTAKER = 8;
-const int INTAKEL = 9;
-
 //Lifter
+// Really? These should be via TalonSRX
 const int ENCODER_A = 12;
 const int ENCODER_B = 13;
 
-//Moose
-const int MOOSEPOSITIONSENSOR = 0;		//Change to Digital Input Output port 0
-										//if available. Chris wired it there
 
-//Solenoids
+/****** Solenoids ************************/
+
 const int INTAKEARML = 0;
 const int INTAKEARMR = 1;
 const int MOOSELIFTER = 2;
 const int MOOSELOCK = 3;		//Controls Antlers Solenoid
 const int MOOSEPREVENT = 4;		//Prevents Moose from going up any further
 
-// CAN Channels
+
+/****** CAN Channels *********************/
+
 const int COMPRESSOR = 1;		//Controls compressor
 const int LIFTER = 2;			//CANTalons CAN ports need to be assigned
 const int PUSHER = 3;			//CANTalons CAN ports need to be assigned
 
-// Analog Input channels
+
+/****** Analog Input channels ************/
+
 const int C_GYRO = 0;
 
-////buttons and axes
+
+/****** Buttons and Axes *****************/
+
 //lifters
 const int A_LIFTER = 1;           //left thumbstick up and down
 const int A_PUSHER = 3;			  //right thumbstick up and down
@@ -63,9 +73,5 @@ const int B_INTAKEIN = 2;		   // bottom button on top of joystick
 const int B_INTAKEROTATECW = 5;    // right button on top of joystick
 const int B_INTAKEOUT = 3;		   // top button on top of joystick
 
-// If you are using multiple modules, make sure to define both the port
-// number and the module. For example you with a rangefinder:
-//const int RANGE_FINDER_PORT = 1;
-//const int RANGE_FINDER_MODULE = 1;
 
 #endif
