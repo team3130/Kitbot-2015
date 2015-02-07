dbb781b91aed9a621934498392e4bcf953c7a60b
Mikhail Kyraha <mikhail@kyraha.com>
2015-02-06 21:42:02 -0600

    Working RobotSensors

--- Kitbot/src/Commands/RobotSensors.h
+++ Kitbot/src/Commands/RobotSensors.h
@@ -2,14 +2,6 @@
 #define ROBOT_SENSORS_H
 
 #include "../CommandBase.h"
-#include "../Subsystems/AntlerMoose.h"
-#include "../Subsystems/Intake.h"
-#include "../Subsystems/IntakeArms.h"
-#include "../Subsystems/Lifter.h"
-#include "../Subsystems/MooseLifter.h"
-#include "../Subsystems/Pusher.h"
-#include "../Subsystems/ExampleSubsystem.h"
-#include "WPILib.h"
 
 class RobotSensors: public CommandBase
 {
