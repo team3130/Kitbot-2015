3e0c79ba12ea32ca12435b0b35176aca49973e16
Duckfan77 <cwenthin@yahoo.com>
2015-03-26 13:09:20 -0500

    Pusher stuttering or constant movement fix
    Changed to use switches plugged into roborio, but added a set speed to
    zero if passed zero piece, and am disabling the limit mode of the pusher
    motor.

--- Kitbot/src/Subsystems/Pusher.h
+++ Kitbot/src/Subsystems/Pusher.h
@@ -17,8 +17,8 @@ public:
 	void InitDefaultCommand();
 	void pushLifter(float speed);
 	void SwitchLED(bool status){m_cInLED->Set(status);};
-	bool GetLimitSwitchOut(){return (m_cPushMotor->IsRevLimitSwitchClosed() == 1);};
-	bool GetLimitSwitchIn(){return (m_cPushMotor->IsFwdLimitSwitchClosed() == 1);};
+	bool GetLimitSwitchOut(){return !m_cSwitchOut->Get();};
+	bool GetLimitSwitchIn(){return !m_cSwitchIn->Get();};
 	int GetDir() {return (m_cPushMotor->Get());};
 
 };
